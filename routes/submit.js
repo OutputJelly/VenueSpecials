7787de8dfa987dd56820980a6ca8369e8be9f60c
Roger Panella <rogerpanella@gmail.com>
2016-01-05 12:06:27 -0600

    started Google Places search

--- routes/submit.js
+++ routes/submit.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/submit', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('submit', { title: 'Express' });
 });
 
 module.exports = router;
