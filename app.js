1819347a42822a897389ae93ce803d5081c2af39
Roger Panella <rogerpanella@gmail.com>
2016-01-05 10:12:11 -0600

    added submit view and route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var submit = require('./routes/submit');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/', submit);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
