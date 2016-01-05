43226c4168e301edab6dd5ae1d83ea3de9eb7dc3
Jason Bratt <jasonhero858@gmail.com>
2016-01-05 10:11:10 -0600

    model fix

--- models/Special.js
+++ models/Special.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var SpecialSchema = new mongoose.Schema({
   VenueID: String,
   Username: String,
-  Description: String
+  Description: String,
+  Date: Timestamp
 });
 
 module.exports = mongoose.model('Special', SpecialSchema, 'specials');
