1c86b3f8acd9341716ac32642c85e1749db01f5c
Sam Vredenburgh <sam.vburgh@gmail.com>
2016-01-06 10:39:47 -0600

    authenticated submit route

--- routes/submit.js
+++ routes/submit.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+  return next();
+  res.redirect('/users/login');
+}
 
 /* GET home page. */
-router.get('/submit', function(req, res, next) {
-  res.render('submit', { title: 'Express' });
+router.get('/submit', isAuthenticated, function(req, res, next) {
+  res.render('submit', { title: 'Express', user: req.user });
 });
+
+
 
 module.exports = router;
