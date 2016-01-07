a55d45ec39f0d60d29ecd7431d72fab27817ee9d
Daniel Tabion <dctabion@gmail.com>
2016-01-07 16:54:27 -0600

    shit

--- db/database.js
+++ db/database.js
@@ -2,14 +2,14 @@ require('dotenv').load();
 
 //database stuff here
 var mongoose = require('mongoose');
-// var connectionString = 'mongodb://localhost/'
-// if(process.env.NODE_ENV === 'production'){
-//   connectionString = process.env.MONGOLAB_URI;
-// }
-var connectionString = process.env.DATABASE_URL || process.env.MONGOLAB_URI;
+var connectionString = 'mongodb://localhost/'
+if(process.env.NODE_ENV === 'production'){
+  connectionString = process.env.MONGOLAB_URI;
+}
+// var connectionString = process.env.DATABASE_URL || process.env.MONGOLAB_URI;
 console.log("-------------------------------Attempting to connect to Database");
-console.log(process.env.MONGOLAB_URI);
-console.log(connectionString);
+console.log('process.evn.MOGOLAB_URI: ' + process.env.MONGOLAB_URI);
+console.log('connection string' + connectionString);
 mongoose.connect(connectionString);
 
 mongoose.connection.on('connected', function() {
