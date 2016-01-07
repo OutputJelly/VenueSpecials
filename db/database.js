69c39866866f2d308707578db98bcd27e23dafcd
Daniel Tabion <dctabion@gmail.com>
2016-01-07 15:55:47 -0600

    update:

--- db/database.js
+++ db/database.js
@@ -1,7 +1,6 @@
 //database stuff here
 var mongoose = require('mongoose');
 
-console.log('process.env.DATABASE_URL:', process.env.DATABASE_URL);
 var connectionString = process.env.DATABASE_URL || process.env.MONGOLAB_URI;
 
 mongoose.connect(connectionString);
