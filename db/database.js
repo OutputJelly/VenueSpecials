92d9654f49b8716e77f452c5954c7ba9ef900357
Daniel Tabion <dctabion@gmail.com>
2016-01-07 13:57:48 -0600

    1) Hide google api key.  2) Hide database connection string

--- db/database.js
+++ db/database.js
@@ -1,6 +1,7 @@
 //database stuff here
 var mongoose = require('mongoose');
 
+console.log('process.env.DATABASE_URL:', process.env.DATABASE_URL);
 var connectionString = process.env.DATABASE_URL || process.env.MONGOLAB_URI;
 
 mongoose.connect(connectionString);
